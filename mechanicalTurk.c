297bc2b0d4443b5d76ebf4a0cadb2c0aa48ba352
JamesMorel <jamesmorel3869@gmail.com>
2016-05-20 15:06:39 +1000

    Add files via upload

---
+++ mechanicalTurk.c
@@ -0,0 +1,43 @@
+/*
+ *  Mr Pass.  Brain the size of a planet!
+ *
+ *  Proundly Created by Richard Buckland
+ *  Share Freely Creative Commons SA-BY-NC 3.0. 
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "Game.h"
+#include "mechanicalTurk.h"
+
+action decideAction (Game g) {
+
+    action nextAction;
+
+    nextAction.actionCode = PASS;
+
+    int currentPlayer = getWhoseTurn(g);
+
+    if(currentPlayer != NO_ONE){
+        int numMJ = getStudents(g, currentPlayer, STUDENT_MJ);
+        int numMTV = getStudents(g, currentPlayer, STUDENT_MTV);
+        int numM$ = getStudents(g, currentPlayer, STUDENT_MMONEY);
+//        int numBPS = getStudents(g, currentPlayer, STUDENT_BPS);
+//        int numBQN = getStudents(g, currentPlayer, STUDENT_BQN);
+
+        if(numMJ >=1 && numMTV >=1 && numM$ >=1){
+            nextAction.actionCode = START_SPINOFF;
+        }
+//         else if(numBPS >=1 && numBQN >=1){
+//            nextAction.actionCode = OBTAIN_ARC;
+//        }
+         else{
+            nextAction.actionCode = PASS;
+        }
+    }
+
+    return nextAction;
+}
