e2ba021bc2f4c770b9ced0d479e3bef17da7509c
JamesMorel <jamesmorel3869@gmail.com>
2016-05-25 17:26:40 +1000

    Update mechanicalTurk.c

--- mechanicalTurk.c
+++ mechanicalTurk.c
@@ -14,28 +14,27 @@
 #include "mechanicalTurk.h"
 
 action decideAction (Game g) {
-
+    int counter = 0;
     action nextAction;
 
     nextAction.actionCode = PASS;
 
     int currentPlayer = getWhoseTurn(g);
+    if(counter <= 20){
+        if(currentPlayer != NO_ONE){
+            int numMJ = getStudents(g, currentPlayer, STUDENT_MJ);
+            int numMTV = getStudents(g, currentPlayer, STUDENT_MTV);
+            int numM$ = getStudents(g, currentPlayer, STUDENT_MMONEY);
+            int numBPS = getStudents(g, currentPlayer, STUDENT_BPS);
+            int numBQN = getStudents(g, currentPlayer, STUDENT_BQN);
 
-    if(currentPlayer != NO_ONE){
-        int numMJ = getStudents(g, currentPlayer, STUDENT_MJ);
-        int numMTV = getStudents(g, currentPlayer, STUDENT_MTV);
-        int numM$ = getStudents(g, currentPlayer, STUDENT_MMONEY);
-//        int numBPS = getStudents(g, currentPlayer, STUDENT_BPS);
-//        int numBQN = getStudents(g, currentPlayer, STUDENT_BQN);
-
-        if(numMJ >=1 && numMTV >=1 && numM$ >=1){
-            nextAction.actionCode = START_SPINOFF;
-        }
-//         else if(numBPS >=1 && numBQN >=1){
-//            nextAction.actionCode = OBTAIN_ARC;
-//        }
-         else{
-            nextAction.actionCode = PASS;
+            if(numMJ >=1 && numMTV >=1 && numM$ >=1){
+                nextAction.actionCode = START_SPINOFF;
+            }else if(numBPS >=1 && numBQN >=1){
+                nextAction.actionCode = OBTAIN_ARC;
+            }else{
+                nextAction.actionCode = PASS;
+                counter++;
         }
     }
 
