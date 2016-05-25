bd288b201b608fa3e8b5b10eb1c28489981bdd01
lisquared <shannonli1209@gmail.com>
2016-05-25 18:59:57 +1000

    Update mechanicalTurk.c

--- mechanicalTurk.c
+++ mechanicalTurk.c
@@ -18,25 +18,62 @@ action decideAction (Game g) {
     action nextAction;
 
     nextAction.actionCode = PASS;
-
+    //debateCampus
+    //debateGO8s
+    //debateEnemies
+    //debateSpinoff
+    int currentTurn = getTurnNumber(g);
     int currentPlayer = getWhoseTurn(g);
-    if(counter <= 20){
-        if(currentPlayer != NO_ONE){
-            int numMJ = getStudents(g, currentPlayer, STUDENT_MJ);
-            int numMTV = getStudents(g, currentPlayer, STUDENT_MTV);
-            int numM$ = getStudents(g, currentPlayer, STUDENT_MMONEY);
-            int numBPS = getStudents(g, currentPlayer, STUDENT_BPS);
-            int numBQN = getStudents(g, currentPlayer, STUDENT_BQN);
-
+    int numMJ = getStudents(g, currentPlayer, STUDENT_MJ);
+    int numMTV = getStudents(g, currentPlayer, STUDENT_MTV);
+    int numM$ = getStudents(g, currentPlayer, STUDENT_MMONEY);
+    int numBPS = getStudents(g, currentPlayer, STUDENT_BPS);
+    int numBQN = getStudents(g, currentPlayer, STUDENT_BQN);    
+        if (currentPlayer != NO_ONE) {
             if(numMJ >=1 && numMTV >=1 && numM$ >=1){
                 nextAction.actionCode = START_SPINOFF;
             }else if(numBPS >=1 && numBQN >=1){
                 nextAction.actionCode = OBTAIN_ARC;
             }else{
                 nextAction.actionCode = PASS;
-                counter++;
+        } else if (currentTurn = 2) {
+            nextAction.actionCode = OBTAIN_ARC;
+        } else if (currentTurn >= 3 && <= 100) {
+            possible = debateCampus(g, currentPlayer);
+            
+            if (possible < 0) {
+                next
+            }
+            
+            
+            
+        }
+    } 
+    }
+    
+    int debateCampus(g, currentPlayer) {
+        int possible = 0;
+        int player = currentPlayer;
+        int campus = getCampuses(g, player);
+        int arcs = getARCs(g, player);
+        
+        if (arcs <= campus) {
+            possible = -1;
+        } else if (campus >= 8) {
+            possible = 0;
+        } else if (numBPS >= 1 && numB? >= 1 && numMJ >= 1 && numMTV >= 1) {
+            possible = 1;
+        } else {
+            possible = 2;
+        }
+        return possible;
+    }
+    
+    
+    int lastResort(g) {
+        if(numMJ >=1 && numMTV >=1 && numM$ >=1) {
+            return 1;
         }
     }
-
     return nextAction;
 }
